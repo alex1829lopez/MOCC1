8e97f766db082392879f12488c8ee1964cbd1e1f
alex1829lopez <alex1829lopez@gmail.com>
2026-06-07 02:27:00 +0000

    fix circular import final

--- auth.py
+++ auth.py
@@ -1,30 +1,3 @@
-def register_user(name, email, password):
-
-    db = SessionLocal()
-
-    try:
-        user_exists = db.query(User).filter(User.email == email).first()
-
-        if user_exists:
-            return False, "El usuario ya existe"
-
-        hashed_password = generate_password_hash(password)
-
-        new_user = User(
-            name=name,
-            email=email,
-            password=hashed_password
-        )
-
-        db.add(new_user)
-        db.commit()
-        db.refresh(new_user)
-
-        return True, "Usuario registrado correctamente"
-
-    except Exception as e:
-        db.rollback()
-        return False, str(e)
-
-    finally:
-        db.close()
+from database import SessionLocal
+from models import User
+from werkzeug.security import generate_password_hash, check_password_hash
